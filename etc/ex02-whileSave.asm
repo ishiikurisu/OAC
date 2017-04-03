a4548e43edafef8980fcf635980f86ea88778b15
Cristiano Silva Jr <cristiano.a.junior@ieee.org>
2017-04-03 08:52:57 -0300

    Getting the while loop problem kinda solved

--- etc/ex02-whileSave.asm
+++ etc/ex02-whileSave.asm
@@ -6,25 +6,43 @@
 #
 main:
 	# TODO Fill "save" array with data
-	# IDEA save must be in $s0, k in $s1, and i in $v0
-	jal while 
+	# Declared save array in .data section
+	li $v0, 5             	# k = scan()
+	syscall
+	add $s1, $0, $v0
+	addi $v0, $0, 0	        # i = 0
+	la $s0, save
+	
+	jal while           # Running the while loop
 	add $a0, $zero, $v0 # Printing output
-	li $v0, 4
+	li $v0, 1
 	syscall
 	
-	li $a0, 10 # Finishing the program
+	li $v0, 10 # Finishing the program
 	syscall
 
 # Simulates the snippet "while(save[i]==k)i++;" with the aid of the continue procedure
-while:
-	# t0 = save[i]
-	# $v0 = i
-	# $s0 = save
-	# $s1 = k
-	lw $t0, $v0($s0)
-	beq $t0, $s1, continue
-	addi $v0, $v2, 1
-	j while
+# $t0 = save[i]
+# $v0 = i
+# $s0 = save
+# $s1 = k
+while:	
+	# Getting offset
+	addi $t0, $0, 4         # Generating a 4
+	mul $t0, $v0, $t0       # Multiplying current offset by 4
+	add $t0, $t0, $s0       # Getting correct address
+	lw $t0, 0($t0)          # Loading address in save + 4*i
+	# BUG What if the user queries a number that is not on the array?
+	
+	# Checking for loop condition
+	beq $t0, $s1, continue  # Exits the loop if the condition is true
+	addi $v0, $v0, 1        # Incrementing i
+	j while                 # Restarting loop
 	
 continue:
 	jr $ra
+
+.data
+	# TODO Create an array with user read values
+	# The array to be iterated on
+	save: .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
