eb53469b6e6784dd8a67e6a18cde6564810baae7
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-04-21 08:27:36 -0300

    Generating plots for best and worst cases

---
+++ labs/lab1/ex1/genfig.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as mlp
+import re
+
+if __name__ == '__main__':
+    n, bc, wc = [ ], [ ], [ ]
+    with open('data.csv', 'r') as fp:
+        for line in fp:
+            values = list(map(int, filter(lambda x: len(x), re.split('\W+', line))))
+            n.append(values[0])
+            bc.append(values[1])
+            wc.append(values[2])
+    mlp.scatter(n, bc)
+    mlp.suptitle('Melhores casos')
+    mlp.savefig('best.png')
+    mlp.clf()
+    mlp.scatter(n, wc)
+    mlp.suptitle('Piores casos')
+    mlp.savefig('worst.png')
+    mlp.clf()
