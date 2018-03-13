f7ae058b6725dc4ceefa3e01b7428740a694595d
Cris Silva Jr <ishiikurisu@yahoo.com>
2018-03-13 14:56:34 -0300

    The MIF format is not as expected

--- src/simulador/main.c
+++ src/simulador/main.c
@@ -1,15 +1,17 @@
+#include <stdlib.h>
 #include "./mips/mips.h"
 
 int main(int argc, char const *argv[]) {
     const char *src = argv[1];
-    unsigned int* instructions = load_from_file(src);
-    unsigned int instruction;
+    unsigned long* instructions = load_from_file(src);
+    unsigned long instruction;
     int i;
 
     for (i = 0; instructions[i] != 0; ++i) {
         instruction = instructions[i];
-        printf("%s\n", instruction_to_string(instruction));
+        printf("%lx\n", instruction);
     }
 
+    free(instructions);
     return 0;
 }
--- src/simulador/mips/mips.h
+++ src/simulador/mips/mips.h
@@ -6,37 +6,39 @@
 
 // Loads a MIF executable file to an array of unsigned integer numbers, each
 // representing an instruction. The last instruction is represented with a 0.
-unsigned int* load_from_file(const char* input)
+unsigned long* load_from_file(const char* input)
 {
     FILE* inlet = fopen(input, "rb");
-    unsigned int* outlet = NULL;
+    unsigned long* outlet = NULL;
+    int limit;
+    int i;
 
-    // TODO Discover file size
-    outlet = (unsigned int*) malloc(sizeof(int) * 2);
-    outlet[0] = 33;
-    outlet[1] = 0;
-
-    // TODO Load whole file
-
+    fseek(inlet, 0L, SEEK_END);
+    limit = ftell(inlet);
+    rewind(inlet);
+    outlet = (unsigned long*) malloc(sizeof(long) * (limit+1));
+    fread(outlet, sizeof(long), limit, inlet);
+    outlet[limit] = 0;
     fclose(inlet);
+
     return outlet;
 }
 
 // Turns an unsigned integer to a string where each character is either 0 or 1,
 // representing the instruction structure. The end of the string is indicated
 // by a NULL pointer. The MSB is the first one by the way.
-char* instruction_to_string(unsigned int instruction)
+char* instruction_to_string(unsigned long instruction)
 {
     char* outlet = NULL;
     int i;
     int result;
     int offset;
 
-    outlet = (char*) malloc(sizeof(char) * 9);
-    outlet[8] = '\0';
-    for (i = 0; i < 8; i++)
+    outlet = (char*) malloc(sizeof(char) * 33);
+    outlet[32] = '\0';
+    for (i = 0; i < 32; i++)
     {
-        result = (outlet[i] >> (8 - i)) & 0x1;
+        result = (outlet[i] >> (32 - i)) & 0x1;
         outlet[i] = (result == 0x1)? '1' : '0';
     }
 
