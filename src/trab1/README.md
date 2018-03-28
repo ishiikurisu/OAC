001fd8215376fa4796d687af5459f145e6a2013d
Cris Silva Jr <ishiikurisu@yahoo.com>
2018-03-28 13:59:48 -0300

    Starting new folder for project 1

---
+++ src/trab1/README.md
@@ -0,0 +1,3 @@
+# Projeto 1
+
+Repositório com o código do trabalho 1 como especificado.
---
+++ src/trab1/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    printf("# Trabalho 1 de OAC 2018/1");
+    return 0;
+}
