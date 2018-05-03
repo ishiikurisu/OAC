3d3042025006e7b575dd364f1317adcba6ed8129
Cristiano Alves da Silva Jr <cristianoalvesjr@gmail.com>
2018-05-03 15:25:49 -0300

    Calling sum and multiplication operations

--- src/trab3/src/main.asm
+++ src/trab3/src/main.asm
@@ -2,8 +2,7 @@
 NUMERO_1: .asciiz "Digite o primeiro numero: "
 NUMERO_2: .asciiz "Digite o segundo numero: "
 OPERACAO: .asciiz "Escolha a operacao (+ ou *): "
-MAIS: .ascii "+"
-VEZES: .ascii "*"
+AVISO: .asciiz "Operacao invalida escolhida!"
 
 .text
 # Menu principal
@@ -36,15 +35,46 @@ syscall
 # Decidindo procedimento
 # ======================
 #
+add $a0, $s0, $zero
+add $a1, $s1, $zero
+add $s2, $v0, $zero
+add $s3, $0, $0
 
-# TODO Comparar `char` em $v0 para saber se a operecao atual eh uma soma
-# TODO Comparar `char` em $v0 para saber se a operecao atual eh uma multiplicao
+# TODO Comparar `char` em $s2 para saber se a operecao atual eh uma soma
+addi $t0, $0, 43
+bne $t0, $s2, NAO_SOME
+  jal SOMAR
+  addi $s3, $s3, 1
+  mtc1 $v0, $f12
+  li $v0, 2
+  syscall
+NAO_SOME:
+nop
+
+# TODO Comparar `char` em $s2 para saber se a operecao atual eh uma multiplicao
+addi $t0, $0, 42
+bne $t0, $s2, NAO_MULTIPLIQUE
+  jal MULTIPLICAR
+  addi $s3, $s3, 1
+  mtc1 $v0, $f12
+  li $v0, 2
+  syscall
+NAO_MULTIPLIQUE:
+nop
+
 # TODO Avisar quando uma operacao invalida foi chamada
+bne $0, $s3, NAO_AVISE
+  li $v0, 4
+  la $a0, AVISO
+  syscall
+NAO_AVISE:
+nop
 
 
 # Saindo programa
 # ===============
 #
+SAIDA:
 li $v0, 10
 syscall
 
@@ -52,4 +82,24 @@ syscall
 # =====================
 #
 
-# TODO Implement the rest of the application
+# Essa operacao realiza a soma de dois numeros em ponto flutuante, sendo um
+# deles em $a0 e o outro em $a1. O resultado estará guardado em $v0.
+#
+SOMAR:
+mtc1 $a0, $f0
+mtc1 $a1, $f1
+# TODO Implementar soma de verdade
+add.s $f0, $f1, $f0
+mfc1 $v0, $f0
+jr $ra
+
+# Essa operacao realiza a soma de dois numeros em ponto flutuante, sendo um
+# deles em $a0 e o outro em $a1. O resultado estará guardado em $v0.
+#
+MULTIPLICAR:
+mtc1 $a0, $f0
+mtc1 $a1, $f1
+# TODO Implementar multiplicacao de verdade
+mul.s $f0, $f1, $f0
+mfc1 $v0, $f0
+jr $ra
