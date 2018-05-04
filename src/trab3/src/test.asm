740bdc4dca86ece0abdb01abaa11bebdafd34226
Cristiano Alves da Silva Jr <cristianoalvesjr@gmail.com>
2018-05-04 14:44:45 -0300

    Extracting exponent from float

--- src/trab3/src/test.asm
+++ src/trab3/src/test.asm
@@ -1,6 +1,6 @@
 .data
 PI: .float -3.14159 # => 0xc0490fd0
-MSG_ERROS: .asciiz "Os testes falharam!\n"
+MSG_ERROS: .asciiz "\nOs testes falharam!\n"
 
 .text
 #
@@ -12,8 +12,7 @@ add $s7, $0, $0
 
 # Lendo um float da memória
 la $t0, PI
-lw $t0, 0($t0)
-ori $s0, $t0, 0x0
+lw $s0, 0($t0)
 
 # Extraindo sinal
 ori $a0, $s0, 0
@@ -22,7 +21,7 @@ li $t0, 0x1
 bne $t0, $v0, FIM
 addi $s7, $s7, 1
 
-# TODO Extrair expoente
+# Extraindo expoente
 ori $a0, $s0, 0
 jal GET_EXP
 li $t0, 0x80
@@ -35,7 +34,7 @@ addi $s7, $s7, 1
 
 # Terminando programa
 FIM:
-beq $s7, $0, SEM_ERROS
+bne $s7, $0, SEM_ERROS
   li $v0, 4
   la $a0, MSG_ERROS
   syscall
@@ -55,6 +54,6 @@ jr $ra
 # Extrai o expoente de um número em ponto flutuante simples guardado em $a0
 # e o guarda em $v0.
 GET_EXP:
-nop
-li $v0, 5
+srl $v0, $a0, 23
+andi $v0, $v0, 0xFF
 jr $ra
