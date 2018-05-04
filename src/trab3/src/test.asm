358202ed601588898885351744795a2f776a07d1
Cristiano Alves da Silva Jr <cristianoalvesjr@gmail.com>
2018-05-04 14:50:55 -0300

    Extracting 'mantissa'

--- src/trab3/src/test.asm
+++ src/trab3/src/test.asm
@@ -1,6 +1,7 @@
 .data
 PI: .float -3.14159 # => 0xc0490fd0
-MSG_ERROS: .asciiz "\nOs testes falharam!\n"
+MSG_ERROS: .asciiz "Os testes falharam!"
+MSG_SEM_ERROS: .asciiz "Todos os testes passaram!"
 
 .text
 #
@@ -28,17 +29,26 @@ li $t0, 0x80
 bne $t0, $v0, FIM
 addi $s7, $s7, 1
 
-# TODO Extrair mantissa
+# Extraindo mantissa
+ori $a0, $s0, 0
+jal GET_MAN
+li $t0, 0x890fd0
+bne $t0, $v0, FIM
+addi $s7, $s7, 1
 
 # TODO Normalizar número
 
 # Terminando programa
 FIM:
+li $v0, 4
 bne $s7, $0, SEM_ERROS
-  li $v0, 4
   la $a0, MSG_ERROS
   syscall
+  j RETURN_0
 SEM_ERROS:
+  la $a0, MSG_SEM_ERROS
+  syscall
+RETURN_0:
 li $v0, 10
 syscall
 
@@ -57,3 +67,10 @@ GET_EXP:
 srl $v0, $a0, 23
 andi $v0, $v0, 0xFF
 jr $ra
+
+# Extrai a mantissa de um número em ponto flutuante simples guardado em $a0
+# e o guarda em $v0.
+GET_MAN:
+andi $v0, $a0, 0x7fffff
+jr $ra
+
