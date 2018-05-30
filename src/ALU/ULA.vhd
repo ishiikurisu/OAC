ad006f4233a1e18969bd2fc6d8bc7ea5635da6dd
Cris Silva Jr <ishiikurisu@yahoo.com>
2018-05-30 15:24:45 -0300

    Learning how to use variables and assert clauses

--- src/ALU/ULA.vhd
+++ src/ALU/ULA.vhd
@@ -3,15 +3,33 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity ULA is
+	generic(
+		WSIZE: natural := 32
+	);
 	port (
-		opcode: in std_logic_vector(4 downto 0);
-		A, B: in std_logic_vector(31 downto 0);
-		Z: out std_logic_vector(31 downto 0);
-		zero, ovfl: out std_logic
+		signal opcode: in std_logic_vector(3 downto 0);
+		signal A, B: in std_logic_vector(WSIZE-1 downto 0);
+		signal Z: out std_logic_vector(WSIZE-1 downto 0);
+		signal zero, ovfl: out std_logic
 	);
 end ULA;
 
 architecture rtl of ULA is
 begin
-	Z <= std_logic_vector(signed(A) + signed(B));
+	working: process(opcode, A, B)
+		variable r: std_logic_vector(WSIZE-1 downto 0);
+	begin
+		if opcode = "0000" then
+			r := std_logic_vector(signed(A) + signed(B));
+		else
+			r := std_logic_vector(to_signed(0, 32));
+		end if;
+			  
+		Z <= r;
+		if r = X"00000000" 
+			then zero <= '1';
+			else zero <= '0';
+		end if;
+		ovfl <= '0';
+	end process;
 end rtl;
--- src/ALU/testbench.vhd
+++ src/ALU/testbench.vhd
@@ -12,11 +12,14 @@ architecture ula_arch of testbench is
 	signal result: std_logic_vector(31 downto 0);
 	signal zero: std_logic;
 	signal overflow: std_logic;
-	signal opcode: std_logic_vector(4 downto 0);
+	signal opcode: std_logic_vector(3 downto 0);
 	
 	component ULA
+		generic (
+			WSIZE: natural := 32
+		);
 		port (
-			opcode: in std_logic_vector(4 downto 0);
+			opcode: in std_logic_vector(3 downto 0);
 			A, B: in std_logic_vector(31 downto 0);
 			Z: out std_logic_vector(31 downto 0);
 			zero, ovfl: out std_logic
@@ -36,13 +39,19 @@ architecture ula_arch of testbench is
 		
 		init: process
 		begin
+			-- Testing sum
+			opcode <= "0000";
 			a <= X"00000004"; b <= X"00000005";
 			wait for 4 ps;
 			assert(result = std_logic_vector(to_signed(9, 32)));
+			assert(zero = '0');
+			
+			a <= X"00000000"; b <= X"00000000";
+			wait for 4 ps;
+			assert(result = std_logic_vector(to_signed(0, 32)));
+			assert(zero = '1');
 			
 			a <= X"00000032"; b <= X"00000005";
-			wait for 4 ps;
-			a <= X"00000004"; b <= X"000000FF";
 			wait for 4 ps;
 		end process init;
 end ula_arch;
